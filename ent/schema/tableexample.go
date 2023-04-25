cd5178a9ae3c8d1da7280d2add9d5cafd5d4d186
akif kadıoğlu <akif.kadioglu.28@gmail.com>
2023-04-25 17:57:53 +0300

    created demo schema

---
+++ ent/schema/tableexample.go
@@ -0,0 +1,29 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// TableExample holds the schema definition for the TableExample entity.
+type TableExample struct {
+	ent.Schema
+}
+
+// Fields of the Table.
+func (TableExample) Fields() []ent.Field {
+	return []ent.Field{
+		field.JSON("field1", FieldStruct{}),
+		field.JSON("field2", []FieldStruct{}),
+	}
+}
+
+type FieldStruct struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+// Edges of the TableExample.
+func (TableExample) Edges() []ent.Edge {
+	return nil
+}
